48f4c3c3ae18eac501e5b0118483e8ccbaa5d84b
Andreas Busold <ab@neopoly.de>
2016-03-22 15:59:42 +0100

    Add action to sign_out current profile

--- app/controllers/registrar_controller.rb
+++ app/controllers/registrar_controller.rb
@@ -1,6 +1,11 @@
 class RegistrarController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def sign_out
+    sign_out_current_profile
+    redirect_to after_sign_out_url
+  end
+
   def callback
     redirect_to after_auth_url
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # Delegate all OmniAuth callbacks to Registrar
   match 'auth/:strategy/callback' => 'registrar#callback', :via => [:get, :post]
+  get 'sign_out' => 'registrar#sign_out'
 end
